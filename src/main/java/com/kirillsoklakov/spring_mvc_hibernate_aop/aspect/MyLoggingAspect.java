0fa7a2aa1cef9b61007ab60a52e6b600935b0308
K-real1995 <garfild17@yandex.ru>
2022-06-23 13:04:55 +0700

    Bug fixing

--- src/main/java/com/kirillsoklakov/spring_mvc_hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/kirillsoklakov/spring_mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -1,19 +1,22 @@
 package com.kirillsoklakov.spring_mvc_hibernate_aop.aspect;
 
+import com.kirillsoklakov.spring_mvc_hibernate_aop.entity.Employee;
 import javassist.bytecode.SignatureAttribute;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class MyLoggingAspect {
 
-    @Around("execution(* com.kirillsoklakov.spring_mvc_hibernate.aop.dao.*.*(..)")
+    @Around("execution(* com.kirillsoklakov.spring_mvc_hibernate_aop.dao.*.*(..))")
     public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws  Throwable{
-        SignatureAttribute methodSignature = (MethodSignature)proceedingJoinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
 
         String methodName = methodSignature.getName();
         System.out.println("Begin of: " + methodName);
