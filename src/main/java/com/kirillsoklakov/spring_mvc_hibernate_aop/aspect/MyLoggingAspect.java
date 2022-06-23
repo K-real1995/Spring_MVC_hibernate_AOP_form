59a59e9c0b397f2459fd28abc133a30a7d81adea
K-real1995 <garfild17@yandex.ru>
2022-06-23 14:38:14 +0700

    Bug fixing

--- src/main/java/com/kirillsoklakov/spring_mvc_hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/kirillsoklakov/spring_mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -1,13 +1,9 @@
 package com.kirillsoklakov.spring_mvc_hibernate_aop.aspect;
 
-import com.kirillsoklakov.spring_mvc_hibernate_aop.entity.Employee;
-import javassist.bytecode.SignatureAttribute;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
